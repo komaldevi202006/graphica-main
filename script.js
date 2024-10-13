b597a9599fdeac56ea969c5dc825098e091e2ce6
komaldevi202006 <komal2005y@gmail.com>
2024-10-13 17:25:07 +0530

    code pushed

---
+++ script.js
@@ -0,0 +1,5 @@
+document.querySelector('.login-btn').addEventListener('click', () => {
+    document.querySelector('.login-modal').style.display = 'flex';
+});
+
+document.querySelector('.login-modal').
